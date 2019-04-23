9a5f639bd8c7d3ce5c9c6019cde1114f8d366fbd
damian.przepiorka@gmail.com <damian.przepiorka@gmail.com>
2019-04-23 16:52:29 +0200

    docs - add github link

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
+    <div class="branding-wrapper">
       <img alt="Vue logo" src="./assets/logo.png">
       <h1>vue-fb-customer-chat</h1>
       <h3>Facebook Customer Chat Plugin for Vue.js</h3>
+    </div>
+    <div class="github">
+      <a target="_blank" :href="repository">
+        <img src="./assets/GitHub-Mark-64px.png" />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-
+const { repository } = require('../package.json')
 export default {
-  name: 'app'
+  name: 'app',
+
+  computed: {
+    repository() {
+      return repository.url.replace('git+', '')
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 *, *::before, *::after {
   box-sizing: border-box;
   margin: 0;
@@ -36,5 +49,39 @@ body {
 
 h3 {
   font-weight: 300;
+}
+
+.github {
+  position: fixed;
+  top: 18pt;
+  right: 18pt;
+  width: 45pt;
+  height: 45pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: '';
+    border-radius: 50%;
+    box-shadow: 0px 8px 16px rgba(0,0,0, 0.1);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  &:hover::before {
+    opacity: 1;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    opacity: 0.25;
+    border-radius: 50%;
+    overflow: hidden;
+  }
 }
 </style>
